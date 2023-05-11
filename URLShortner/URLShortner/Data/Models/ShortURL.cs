5db4d164af5051250aae704d62b656ecfdce422c
BanzaiMini <banzaimini8@gmail.com>
2023-05-11 22:08:19 +0100

    Add a basic web app that allows unvalidated entries to be posted.

---
+++ URLShortner/URLShortner/Data/Models/ShortURL.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace URLShortner.Data.Models
+{
+    /// <summary>
+    /// Data Model Object representing the mapping between a short url key and the corresponding full URL
+    /// </summary>
+    public class ShortURL
+    {
+        /// <summary>
+        /// The short URL key that maps to a full URL
+        /// </summary>
+        [Key]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// The full URL to redirect to.
+        /// </summary>
+        [Required]
+        public string URL { get; set; }
+
+        /// <summary>
+        /// The generated URL.
+        /// This is not stored only included for display
+        /// </summary>
+        [NotMapped]
+        public string GeneratedURL { get; set; }
+    }
+}
---
+++ URLShortner/URLShortner/Pages/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using URLShortner.Data.Models;
+
+namespace URLShortner.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        
+        /// <summary>
+        /// The values related to the Short URL
+        /// </summary>
+        [BindProperty]
+        public ShortURL ShortURL { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Retrieve any existing URL.
+
+                ShortURL existingEntry = null;
+
+                if (existingEntry != null)
+                {
+                    ModelState.AddModelError("", "The short URL you have entered is not available - please try again.");
+                }
+
+                ShortURL.GeneratedURL = Request.GetDisplayUrl() + ShortURL.Key;
+
+                //TODO: Store the ShortURL
+
+            }
+            else
+            {
+                // The values entered failed validation so redisplay hte page with the validation summary.
+                ModelState.AddModelError("","The values you have entered are not valid - please try again.");
+            }
+
+            return Page();
+        }
+
+
+
+    }
+}
